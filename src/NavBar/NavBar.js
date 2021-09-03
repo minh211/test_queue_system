781541b4c27d076c01b16bb466be1e53f8248e0e
minhlgcd17135 <leminh210199@gmail.com>
2021-09-03 16:14:44 +0700

    Create Doctors role - postgreSQL/sequelizeCLI

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -6,7 +6,7 @@ function NavBar(props) {
   return (
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
       <h5 className="my-0 mr-md-auto font-weight-normal text-danger">
-        Hospital Queueing Management
+          Hospital Automated Queueing System
       </h5>
       <nav className="my-2 my-md-0 mr-md-3">
         <Link
